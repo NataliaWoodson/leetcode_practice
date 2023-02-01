5d93e971ba151c6c1dbc124573a25b335df01d4a
Natalia <natalia.woodson@gmail.com>
2023-02-01 13:13:55 -0500

    add solution remove-element

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -1,15 +1,14 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+def removeDuplicates(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
 
-        left = 1
+    left = 1
 
-        for r in range(1, len(nums)):
-            if nums[r] != nums[r - 1]:
-                nums[left] = nums[r]
-                left += 1
-        
-        return left
+    for r in range(1, len(nums)):
+        if nums[r] != nums[r - 1]:
+            nums[left] = nums[r]
+            left += 1
+    
+    return left
---
+++ remove_element.py
@@ -0,0 +1,14 @@
+def removeElement(self, nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    
+    """
+
+    k = 0
+    for n in range(len(nums)):
+        if nums[n] != val:
+            nums[k] = nums[n]
+            k +=1 
+    return k
